cf8c5be093221137109fa6475cbd73974fdb4e92
asuspipi <6418006+conchyliculture@users.noreply.github.com>
2020-04-10 11:01:57 +0200

    -space

--- win/NvapiFansLib/NvapiFansLib.cpp
+++ win/NvapiFansLib/NvapiFansLib.cpp
@@ -255,7 +255,7 @@ int NvApiClient::getGPUTemperature(NV_PHYSICAL_GPU_HANDLE handle) const {
 bool NvApiClient::detectI2CDevice(NV_PHYSICAL_GPU_HANDLE handle) const {
 	bool res;
 	// Asus GPUTweakII detection mechanism
-	NvU8 high ;
+	NvU8 high;
 	NvU8 low;
 	int chipId = 0;
 
